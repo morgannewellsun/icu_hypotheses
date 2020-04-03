bcf5477bf1112bdec4b1081bca5f31db8926c4a2
dchaebae <dkchae@princeton.edu>
2020-04-03 01:48:08 -0400

    build in control into hypothesis test generation

--- retain-keras/create_fake_data/generate_fake_interact_hypothesis.py
+++ retain-keras/create_fake_data/generate_fake_interact_hypothesis.py
@@ -8,15 +8,13 @@ from sklearn.model_selection import train_test_split
 
 if __name__ == '__main__':
     out_directory = sys.argv[1]
-    N_str = sys.argv[2]
-    train_proportion = float(sys.argv[3])
+    N = int(sys.argv[2])
+    interact_flag = int(sys.argv[3]) # 1 for med1 only, 2 for med2 only, anything else for med1 and med2
 
     # data point t
     TIME = 30
     OFFSET = 4
     DOSAGE = 5
-
-    N = int(N_str)
 
     patients = []
     morts = []
@@ -26,11 +24,10 @@ if __name__ == '__main__':
         health = []
 
         health.append(np.random.normal() - OFFSET) # come in with lower health status
-
         patient = []
         # generate the latent stats from medication
         # one visit
-        for i in range(1, TIME*4):
+        for i in range(1, TIME):
             visit = []
             med1 = max(np.random.normal(DOSAGE, 1), 0)
             med2 = max(np.random.normal(DOSAGE, 1), 0)
@@ -38,18 +35,16 @@ if __name__ == '__main__':
             code1 = min(max(0, int(med1*4)-11), 17)
             code2 = min(max(18, int(med2*4)+7), 36)
 
-            if i < TIME:
-                health.append(np.random.normal(health[i-1] + 0.3 * avgmed, 0.1))
-                visit.append(code1)
-                visit.append(code2)
-            elif i < TIME*2:
+            if interact_flag == 1:
                 health.append(np.random.normal(health[i-1] + 0.1 * med1, 0.1))
                 visit.append(code1)
-            elif i < TIME*3:
+            elif interact_flag == 2:
                 health.append(np.random.normal(health[i-1] - 0.1 * med2, 0.1))
                 visit.append(code2)
             else:
-                health.append(health[-1])
+                health.append(np.random.normal(health[i-1] + 0.3 * avgmed, 0.1))
+                visit.append(code1)
+                visit.append(code2)
             
             if code1 not in types:
                 types[code1] = code1
@@ -68,24 +63,34 @@ if __name__ == '__main__':
         if not mort:
             morts.append(0)
 
-    interact_data = pd.DataFrame(data={'codes': patients[:TIME]}, columns=['codes']).reset_index()
-    interact_target = pd.DataFrame(data={'target': morts[:TIME]},columns=['target']).reset_index()
-    interact_data.sort_index().to_pickle(out_directory+'/interact_data.pkl')
-    interact_target.sort_index().to_pickle(out_directory+'/interact_target.pkl')
+    # clone them for control (doing nothing vs giving treatment)
+    for n in range(N):
+        health2 = []
 
-    med1_data = pd.DataFrame(data={'codes': patients[TIME:TIME*2]}, columns=['codes']).reset_index()
-    med1_target = pd.DataFrame(data={'target': morts[TIME:TIME*2]},columns=['target']).reset_index()
-    med1_data.sort_index().to_pickle(out_directory+'/med1_data.pkl')
-    med1_target.sort_index().to_pickle(out_directory+'/med1_target.pkl')
+        health2.append(health[0])
+        patient = []
+        # generate the latent stats from medication
+        # one visit
+        for i in range(1, TIME):
+            visit = []
+            health2.append(health2[-1])
+            
+            patient.append(visit)
 
-    med2_data = pd.DataFrame(data={'codes': patients[TIME*2:TIME*3]}, columns=['codes']).reset_index()
-    med2_target = pd.DataFrame(data={'target': morts[TIME*2:TIME*3]},columns=['target']).reset_index()
-    med2_data.sort_index().to_pickle(out_directory+'/med2_data.pkl')
-    med2_target.sort_index().to_pickle(out_directory+'/med2_target.pkl')
+            # sigmoid on the latent health status
+            mort = np.random.binomial(1, 1/(1+math.exp(health2[i]/2+OFFSET+1)))
+            if mort:
+                morts.append(1)
+                break
+        
+        patients.append(patient)
+        if not mort:
+            morts.append(0)
 
-    control_data = pd.DataFrame(data={'codes': patients[TIME*3:]}, columns=['codes']).reset_index()
-    control_target = pd.DataFrame(data={'target': morts[TIME*3]},columns=['target']).reset_index()
-    control_data.sort_index().to_pickle(out_directory+'/control_data.pkl')
-    control_target.sort_index().to_pickle(out_directory+'/control_target.pkl')
+    interact_data = pd.DataFrame(data={'codes': patients}, columns=['codes']).reset_index()
+    interact_target = pd.DataFrame(data={'target': morts},columns=['target']).reset_index()
+    print(interact_data.shape)
+    #interact_data.sort_index().to_pickle(out_directory+'/interact_data.pkl')
+    #interact_target.sort_index().to_pickle(out_directory+'/interact_target.pkl')
 
-    pickle.dump(types, open(out_directory+'/dictionary.pkl', 'wb'), -1)
+    #pickle.dump(types, open(out_directory+'/dictionary.pkl', 'wb'), -1)
