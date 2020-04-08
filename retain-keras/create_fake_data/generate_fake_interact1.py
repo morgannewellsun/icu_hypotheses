502430c95c13dc73a00d8577def97971d1ef5fe6
dchaebae <dkchae@princeton.edu>
2020-04-08 12:41:21 -0400

    add Logistic Regression import

--- retain-keras/create_fake_data/generate_fake_interact1.py
+++ retain-keras/create_fake_data/generate_fake_interact1.py
@@ -5,6 +5,7 @@ import math
 import matplotlib.pyplot as plt
 import sys
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 
 if __name__ == '__main__':
     out_directory = sys.argv[1]
