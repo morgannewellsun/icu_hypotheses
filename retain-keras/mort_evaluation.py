766acb744477e8fb03d0004453a143c3f4e5bf84
dchaebae <dkchae@princeton.edu>
2020-05-07 02:42:50 -0400

    sort out the med2 nad both case

--- retain-keras/mort_evaluation.py
+++ retain-keras/mort_evaluation.py
@@ -5,12 +5,19 @@ import argparse
 def main(ARGS):
 
 	y_train = pd.read_pickle(ARGS.path_target)['target'].values
-	print('%d expired out of %d' % (np.sum(y_train), len(y_train)))
+	total_mort = np.sum(y_train)
+	if ARGS.doubled:
+		y_train = y_train[::2]
+		print('Case 1: %d expired out of %d' % (np.sum(y_train), int(len(y_train)/2)))
+		print('Case 2: %d expired out of %d' % (total_mort - np.sum(y_train), int(len(y_train)/2)))
+	else:
+		print('%d expired out of %d' % (total_mort, len(y_train)))
 
 def parse_arguments(parser):
     """Read user arguments"""
     parser.add_argument('--path_target', type=str, default='data/target_train.pkl',
                         help='Path to target')
+    parser.add_argument('--doubled', type=bool, default=False, help='Split double-ups (med2 and both case)')
     args = parser.parse_args()
 
     return args
