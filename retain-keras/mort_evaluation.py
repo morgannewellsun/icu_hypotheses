4b55a168524412d99234730e7a8717ab97c0f9f4
dchaebae <dkchae@princeton.edu>
2020-05-07 02:35:48 -0400

    add a ARG input to mort counter

--- retain-keras/mort_evaluation.py
+++ retain-keras/mort_evaluation.py
@@ -1,5 +1,21 @@
 import pandas as pd
 import numpy as np
 
-y_train = pd.read_pickle(ARGS.path_target_train)['target'].values
-print('%d expired out of %d' % (np.sum(y_train), len(y_train)))
+def main(ARGS):
+
+	y_train = pd.read_pickle(ARGS.path_target)['target'].values
+	print('%d expired out of %d' % (np.sum(y_train), len(y_train)))
+
+def parse_arguments(parser):
+    """Read user arguments"""
+    parser.add_argument('--path_target', type=str, default='data/target_train.pkl',
+                        help='Path to target')
+    args = parser.parse_args()
+
+    return args
+
+if __name__ == '__main__':
+
+    PARSER = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    ARGS = parse_arguments(PARSER)
+    main(ARGS)
