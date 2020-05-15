19c11626ece7c43b08fd82ce88ad861039c63e64
Daniel Chae <dchaebae@daniels-mbp.myfiosgateway.com>
2020-05-15 16:28:25 -0400

    folder restructure

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# icu_hypotheses
+# ICU Hypothesis Project
+
+This is the repository for generating and testing complex hypotheses using the RETAIN model ([Choi et al. 2017](https://arxiv.org/abs/1608.05745)) and a custom attention-based LSTM generator.
+
+## Run code
+### Package requirements
+- Python 3.x
+- Tensorflow
+- Keras
+- Scikit-learn
+- NumPy
+- Pandas
+- Matplotlib
+
+### Hardware requirement
+The code was run using tigergpu. Training RETAIN locally is not recommended. Training the generator is feasible on the local machine.
+
+### RETAIN Predictor
+To train: ```python retain_train.py --num_codes=N --emb_size=M --path_data_train=/path/to/train/data.pkl --path_data_test=/path/to/test/data.pkl --path_target_train=/path/to/train/target.pkl --path_target_test=/path/to/test/target.pkl --directory=/output/directory```
+
+### LSTM Generator
+
+
+## Contributors
+- Daniel Chae (Princeton Class of 2020), advised by Michael Guerzhoy
--- fake_data/generate_fake_interact1.py
+++ fake_data/generate_fake_interact1.py
@@ -5,23 +5,20 @@ import math
 import matplotlib.pyplot as plt
 import sys
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 
 if __name__ == '__main__':
     out_directory = sys.argv[1]
-    N_str = sys.argv[2]
-    train_proportion = float(sys.argv[3])
+    N = int(sys.argv[2])
+    TIME = int(sys.argv[3])
+    train_proportion = float(sys.argv[4])
 
-    # data point t
-    TIME = 30
     OFFSET = 4
     DOSAGE = 5
 
-    N = int(N_str)
-
-    code_placeholder=np.zeros((N, TIME, 4))
-
     patients = []
     morts = []
+    logreg_patients = np.zeros((N, TIME))
     types = {}
     # for every patient
     for n in range(N):
@@ -30,23 +27,40 @@ if __name__ == '__main__':
         health.append(np.random.normal() - OFFSET) # come in with lower health status
 
         patient = []
+        logreg_patient = np.zeros(TIME*2)
         # generate the latent stats from medication
         # one visit
         for i in range(1, TIME):
             visit = []
+            admin = np.random.binomial(1, 0.5, (2,1))[:,0]
             med1 = max(np.random.normal(DOSAGE, 1), 0)
             med2 = max(np.random.normal(DOSAGE, 1), 0)
+            avgmed = (med1 + med2) / 2
             code1 = min(max(0, int(med1*4)-11), 17)
-            code2 = min(max(18, int(med2*4)+7), 36)
-            visit.append(code1)
-            visit.append(code2)
+            code2 = min(max(18, int(med2*4)+7), 35)
+
+            if admin[0] and admin[1]:
+                health.append(np.random.normal(health[i-1] + 0.3 * avgmed, 0.1))
+                visit.append(code1)
+                visit.append(code2)
+                logreg_patient[2*i] = med1
+                logreg_patient[2*i+1] = med2
+            elif admin[0]:
+                health.append(np.random.normal(health[i-1] + 0.1 * med1, 0.1))
+                visit.append(code1)
+                logreg_patient[2*i] = med1
+            elif admin[1]:
+                health.append(np.random.normal(health[i-1] - 0.1 * med2, 0.1))
+                visit.append(code2)
+                logreg_patient[2*i] = 0.
+            else:
+                health.append(health[-1])
+            
             if code1 not in types:
                 types[code1] = code1
             if code2 not in types:
                 types[code2] = code2
             
-            health.append(np.random.normal(health[i-1] + 0.12 * med1 - 0.1 * med2, 0.1))
-
             patient.append(visit)
 
             # sigmoid on the latent health status
@@ -56,9 +70,22 @@ if __name__ == '__main__':
                 break
         
         patients.append(patient)
+        logreg_patients[n, :] = logreg_patient[:TIME]
         if not mort:
             morts.append(0)
 
+    
+    # for logistic regression
+    logreg_train, logreg_test = train_test_split(logreg_patients, train_size=train_proportion, random_state=12345)
+    logreg_train_label, logreg_test_label = train_test_split(morts, train_size=train_proportion, random_state=12345)
+    print('Record the following information:')
+    print('%d expired out of %d'%(np.sum(morts), N))
+    print('%d expired out of %d (test set)'%(np.sum(logreg_test_label), np.shape(logreg_test_label)[0]))
+    logreg_model = LogisticRegression().fit(logreg_train, logreg_train_label)
+    mean_accuracy = logreg_model.score(logreg_test, logreg_test_label)
+    print('Mean accuracy: %f' % mean_accuracy)
+
+    # for RNN
     all_data = pd.DataFrame(data={'codes': patients}, columns=['codes']).reset_index()
     all_targets = pd.DataFrame(data={'target': morts},columns=['target']).reset_index()
 
